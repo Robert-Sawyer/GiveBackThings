0086663e48a553f104ea020a8722408184703f91
Robert Olbrychowski <robert.olbrychowski@hotmail.com>
2019-05-07 17:52:47 +0200

    Added User entity and modified UserDTO.

---
+++ src/main/java/com/github/robertsawyer/GiveBackThings/domain/model/User.java
@@ -0,0 +1,44 @@
+package com.github.robertsawyer.GiveBackThings.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String login;
+    @Column
+    private String firstName;
+    @Column
+    private String lastName;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) &&
+                Objects.equals(login, user.login);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, login);
+    }
+}
--- src/main/java/com/github/robertsawyer/GiveBackThings/dtos/UserDTO.java
+++ src/main/java/com/github/robertsawyer/GiveBackThings/dtos/UserDTO.java
@@ -10,6 +10,5 @@ import lombok.NoArgsConstructor;
 public class UserDTO {
 
     private String login;
-
-
+    
 }
