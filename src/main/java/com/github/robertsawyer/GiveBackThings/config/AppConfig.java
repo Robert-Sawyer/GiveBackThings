b480b26ab4ed1918a0d7b9ca9bf1f897a1eacf42
Robert Olbrychowski <robert.olbrychowski@hotmail.com>
2019-05-07 16:02:29 +0200

    * Modified Configuration files. * Added dependencies - spring security.

--- src/main/java/com/github/robertsawyer/GiveBackThings/config/AppConfig.java
+++ src/main/java/com/github/robertsawyer/GiveBackThings/config/AppConfig.java
@@ -3,14 +3,40 @@ package com.github.robertsawyer.GiveBackThings.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.LocaleContextResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import javax.validation.Validator;
+import java.util.Locale;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "com.github.robertsawyer.GiveBackThings")
-public class AppConfig {
+@EnableTransactionManagement
+@ComponentScan(basePackages = "com.github.robertsawyer.GiveBackThings") 
+@EnableJpaRepositories(basePackages = "com.github.robertsawyer.GiveBackThings.domain.repositories")
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public LocalEntityManagerFactoryBean entityManagerFactory() {
+        LocalEntityManagerFactoryBean emfb = new LocalEntityManagerFactoryBean();
+        emfb.setPersistenceUnitName("giveBackThingsPersistenceUnit");
+        return emfb; }
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager tm = new JpaTransactionManager(emf);
+        return tm; }
 
     @Bean
     public ViewResolver viewResolver() {
@@ -19,4 +45,18 @@ public class AppConfig {
         viewResolver.setPrefix("/WEB-INF/views/");
         viewResolver.setSuffix(".jsp");
         return viewResolver; }
+
+    @Bean(name="localeResolver")
+    public LocaleContextResolver getLocaleContextResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("pl","PL"));
+        return localeResolver; }
+
+    @Bean
+    public Validator validator() {
+        return new LocalValidatorFactoryBean();
+    }
+
+
 }
+
