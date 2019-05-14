8719c01687f7a7c85e5ac9c7ad86473dad44557b
Robert Olbrychowski <robert.olbrychowski@hotmail.com>
2019-05-14 11:36:53 +0200

    Added trusted institutions list view.

--- src/main/java/com/github/robertsawyer/GiveBackThings/controller/TrustedInstitutionController.java
+++ src/main/java/com/github/robertsawyer/GiveBackThings/controller/TrustedInstitutionController.java
@@ -1,9 +1,12 @@
 package com.github.robertsawyer.GiveBackThings.controller;
 
+import com.github.robertsawyer.GiveBackThings.dtos.DeleteInstitutionDTO;
 import com.github.robertsawyer.GiveBackThings.services.TrustedInstitutionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TrustedInstitutionController {
@@ -14,9 +17,16 @@ public class TrustedInstitutionController {
         this.trustedInstitutionService = trustedInstitutionService;
     }
 
-    @GetMapping
+    @GetMapping("/trustedInstitutions")
     public String showInstitutionsList(Model model){
         model.addAttribute("institutions", trustedInstitutionService.getAllTrustedIntitutions());
         return "trustedInstitutions/trustedInstitutionsList";
     }
+
+    @PostMapping(value = "/trustedIntitutions", params = "delete")
+    public String deleteInstitution(@ModelAttribute("deleteIntitution") DeleteInstitutionDTO deleteInstitutionDTO){
+        trustedInstitutionService.deleteInstitution(deleteInstitutionDTO);
+        return "redirect:/trustedInstitutions";
+    }
+
 }
