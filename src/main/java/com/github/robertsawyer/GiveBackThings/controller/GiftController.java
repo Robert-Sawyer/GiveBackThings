26f244b9200962fb8aa391f6b05dfa3c3ca47f96
Robert Olbrychowski <robert.olbrychowski@hotmail.com>
2019-05-16 11:42:41 +0200

    Added actions for showing every step of form (add new gift).

--- src/main/java/com/github/robertsawyer/GiveBackThings/controller/GiftController.java
+++ src/main/java/com/github/robertsawyer/GiveBackThings/controller/GiftController.java
@@ -1,18 +1,33 @@
 package com.github.robertsawyer.GiveBackThings.controller;
 
+import com.github.robertsawyer.GiveBackThings.dtos.AddGiftStepOneDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class GiftController {
 
 
     @GetMapping("/gifts/add/stepOne")
-    public String showAddGiftFormStepOne(){
-
+    public String showAddGiftFormStepOne(Model model){
+        model.addAttribute("stepOne", new AddGiftStepOneDTO());
         return "gifts/giveAGiftStepOne";
     }
 
+    @PostMapping("/gifts/add/stepOne")
+    public String goToStepTwo(@Valid @ModelAttribute("stepOne") AddGiftStepOneDTO addGiftStepOneDTO, BindingResult result){
+        if (result.hasErrors()){
+            return "gifts/giveAGiftStepOne";
+        }
+
+        return "redirect:/gifts/add/stepTwo";
+    }
 
 
     @GetMapping("/gifts/add/stepTwo")
