436683bc4fd36dccbf4c70884b845c77eb023cdb
Robert Olbrychowski <robert.olbrychowski@hotmail.com>
2019-05-26 18:38:06 +0200

    Added one class dto for all steps of adding gift form.

--- src/main/java/com/github/robertsawyer/GiveBackThings/dtos/AddGiftFormDTO.java
+++ src/main/java/com/github/robertsawyer/GiveBackThings/dtos/AddGiftFormDTO.java
@@ -1,6 +1,8 @@
 package com.github.robertsawyer.GiveBackThings.dtos;
 
+import com.github.robertsawyer.GiveBackThings.domain.model.Address;
 import com.github.robertsawyer.GiveBackThings.domain.model.Category;
+import com.github.robertsawyer.GiveBackThings.domain.model.TrustedInstitution;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,9 +13,20 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class AddGiftStepOneDTO {
+public class AddGiftFormDTO {
 
     @NotNull
     private List<Category> category;
 
+    @NotNull
+    private Integer bags;
+
+    @NotNull
+    private TrustedInstitution institution;
+
+    @NotNull
+    private Address address;
+
+    @NotNull
+    private String date;
 }
