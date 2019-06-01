058e7b21dc6cdb35a6056a99702d37a5fac3f8ac
Robert Olbrychowski <robert.olbrychowski@hotmail.com>
2019-06-01 16:26:30 +0200

    Added methods findById in Location and Purpose Repository.

--- src/main/java/com/github/robertsawyer/GiveBackThings/domain/repositories/LocationRepository.java
+++ src/main/java/com/github/robertsawyer/GiveBackThings/domain/repositories/LocationRepository.java
@@ -2,7 +2,11 @@ package com.github.robertsawyer.GiveBackThings.domain.repositories;
 
 import com.github.robertsawyer.GiveBackThings.domain.model.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface LocationRepository extends JpaRepository<Location, Long> {
 
+    @Query(value = "SELECT * FROM locations where id = ?",
+    nativeQuery = true)
+    Location findById(Long locationId);
 }
--- src/main/java/com/github/robertsawyer/GiveBackThings/domain/repositories/PurposeRepository.java
+++ src/main/java/com/github/robertsawyer/GiveBackThings/domain/repositories/PurposeRepository.java
@@ -2,7 +2,11 @@ package com.github.robertsawyer.GiveBackThings.domain.repositories;
 
 import com.github.robertsawyer.GiveBackThings.domain.model.Purpose;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PurposeRepository extends JpaRepository<Purpose, Long> {
 
+    @Query(value = "SELECT * FROM purposes WHERE id = ?",
+    nativeQuery = true)
+    Purpose findById(Long purposeId);
 }
