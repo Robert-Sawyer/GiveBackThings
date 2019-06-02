4197ce04724910235382f5076b95c0d5fb38a05f
Robert Olbrychowski <robert.olbrychowski@hotmail.com>
2019-06-02 13:44:25 +0200

    Added new functionality - making User an Admin.

--- src/main/java/com/github/robertsawyer/GiveBackThings/controller/UserController.java
+++ src/main/java/com/github/robertsawyer/GiveBackThings/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.github.robertsawyer.GiveBackThings.controller;
 
+import com.github.robertsawyer.GiveBackThings.domain.model.User;
 import com.github.robertsawyer.GiveBackThings.dtos.DeleteUserDTO;
+import com.github.robertsawyer.GiveBackThings.dtos.EntitleAdminDTO;
 import com.github.robertsawyer.GiveBackThings.services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -26,6 +28,12 @@ public class UserController {
     public String showUsersList(Model model){
         model.addAttribute("users", userService.getAllUser());
         return "users/usersList";
+    }
+
+    @PostMapping(value = "/usersList", params = "entitle")
+    public String entitleAdmin(@ModelAttribute("entitleUser") EntitleAdminDTO entitleAdmin) {
+        userService.entitleAdmin(entitleAdmin);
+        return "redirect:/adminsList";
     }
 
     @PostMapping(value = "/usersList", params = "delete")
