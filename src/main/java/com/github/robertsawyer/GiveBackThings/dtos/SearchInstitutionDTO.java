bfd52d0aea72c79f9d158c9a57a839dbf0596842
Robert Olbrychowski <robert.olbrychowski@hotmail.com>
2019-06-03 18:20:22 +0200

    Modified fields in SearchInstitutionDTO.

--- src/main/java/com/github/robertsawyer/GiveBackThings/dtos/SearchInstitutionDTO.java
+++ src/main/java/com/github/robertsawyer/GiveBackThings/dtos/SearchInstitutionDTO.java
@@ -1,22 +1,17 @@
 package com.github.robertsawyer.GiveBackThings.dtos;
 
-import com.github.robertsawyer.GiveBackThings.domain.model.Location;
-import com.github.robertsawyer.GiveBackThings.domain.model.Purpose;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotNull;
-import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class SearchInstitutionDTO {
 
-    @NotNull
-    private Location Location;
-    @NotNull
-    private List<Purpose> purpose;
+    private Long locationId;
+
+    private Long purposeId;
 
 }
