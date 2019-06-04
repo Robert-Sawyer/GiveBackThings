e95f6a098ff68fb71d03dd5393c8231719e0a29b
Robert Olbrychowski <robert.olbrychowski@hotmail.com>
2019-06-04 13:08:48 +0200

    Added entity UserDetails.

---
+++ src/main/java/com/github/robertsawyer/GiveBackThings/domain/model/UserDetails.java
@@ -0,0 +1,22 @@
+package com.github.robertsawyer.GiveBackThings.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_details")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Long phoneNumber;
+}
